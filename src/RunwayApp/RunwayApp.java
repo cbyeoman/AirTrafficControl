53a7c6882f94fb4f7c649792d41b8884d2fa40e6
Chris Yeomans <christopherbyeomans@gmail.com>
2017-04-04 23:50:04 -0400

    Added new classes to main method in runway app

--- src/RunwayApp/RunwayApp.java
+++ src/RunwayApp/RunwayApp.java
@@ -16,17 +16,69 @@ public class RunwayApp {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        Runway theRunway = new Runway();
-      theRunway.addPlane("Delta 1023", 560);
-      theRunway.addPlane("Delta 1023", 460);
+       System.out.println("Runway Array");
+            RunwayArray theRunway = new RunwayArray();
 
-      System.out.println("Runway 101 has the following time schedule: \n");
 
-      theRunway.displayPlanes();
-      System.out.println();
-      System.out.println("Runway 101 has the following time schedule: \n");
-      theRunway.insertionSort();
-      theRunway.displayPlanes();
-    }
+            theRunway.addPlane("Delta 1023", 2);
+
+            theRunway.addPlane("Delta 1023", 4);
+
+            theRunway.addPlane("Delta 1023", 3);
+
+
+            theRunway.addPlane("Delta 1023", 500);
+
+            theRunway.addPlane("Delta 1023", 498);
+
+            theRunway.addPlane("Delta 1023", 4);
+
+
+            theRunway.addPlane("Delta 1023", 495);
+            System.out.println("Runway Array has the following time schedule: ");
+            theRunway.displayPlanes();
+
+            System.out.println();
+            System.out.println("New Runway Hashtable");
+                RunwayHashTable theRunway2 = new RunwayHashTable();
+         
+
+
+
+            theRunway2.addPlane("Delta 1023", 492);
+            theRunway2.addPlane("Delta 1023", 494);
+
+            System.out.println("Runway Hashtable has the following time schedule: ");
+
+            theRunway2.displayPlanes();
+
+            System.out.println();
+
+            System.out.println("New Runway BST");
+               RunwayBST theRunway3 = new RunwayBST();
+
+
+            theRunway3.addPlane("Delta 1023", 4);
+
+            theRunway3.addPlane("Delta 1023", 3);
+
+
+            theRunway3.addPlane("Delta 1023", 500);
+
+            theRunway3.addPlane("Delta 1023", 498);
+
+            theRunway3.addPlane("Delta 1023", 4);
+
+
+            theRunway3.addPlane("Delta 1023", 495);
+
+            System.out.println("Runway BST has the following time schedule: ");
+
+            theRunway3.displayPlanes();
+
+
+
+
+        } // end main()
     
 }
