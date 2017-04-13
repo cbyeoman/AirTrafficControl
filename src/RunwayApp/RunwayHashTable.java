9528959d0f911c2d305cd19ddb9a0fff6487f711
Chris Yeomans <christopherbyeomans@gmail.com>
2017-04-13 13:21:45 -0400

    edited hashtable to work with interface

--- src/RunwayApp/RunwayHashTable.java
+++ src/RunwayApp/RunwayHashTable.java
@@ -12,26 +12,27 @@ import java.util.Map;
  *
  * @author Christopher
  */
-public class RunwayHashTable {
-     final int MAX_SLOTS = 100;
+class RunwayHashTable implements Runway // runway hashtable
+{
+
+    final int MAX_SLOTS = 1000;
     final int TIME_INTERVAL = 3;
     int numPlanes = 0;
     // using built-in Java hash table
-    Hashtable < Integer, Plane > hashtable =
-        new Hashtable < Integer, Plane > ();
+    Hashtable < Integer,
+    Plane > hashtable = new Hashtable < Integer,
+    Plane > ();
 
     // -------------------------------------------------------------
     public RunwayHashTable() // constructor
-        {
-            numPlanes++;
-            hashtable.put(1, new Plane("Delta Inaugural Flight", 1));
-            System.out.println("added Delta Inaugural Flight @ time 1");
+    {
+        numPlanes++;
+        hashtable.put(1, new Plane("Delta Inaugural Flight", 1));
+        System.out.println("added Delta Inaugural Flight @ time 1");
 
-        }
+    }
 
-
-    public void addPlane(String name, int time) {
-
+    public String addPlane(String name, int time) {
 
         int minKey = 1;
         int maxKey = 10000;
@@ -39,11 +40,12 @@ public class RunwayHashTable {
         for (Integer key: hashtable.keySet()) {
             //System.out.println("The Hashtable contains key "+ key);
 
-
             if (key < time && key > minKey) {
                 minKey = key;
             } else if (key > time && key < maxKey) {
                 maxKey = key;
+            } else if (key == time) {
+                return "time conflict @ time " + time;
             }
 
         }
@@ -51,26 +53,26 @@ public class RunwayHashTable {
         //System.out.println("The Hashtable smallest "+ minKey);
         //System.out.println("The Hashtable largest "+ maxKey);
 
-
         // if the time found is not the last time in the array
         if (Math.abs(maxKey - time) >= TIME_INTERVAL && Math.abs(minKey - time) >= TIME_INTERVAL) {
             hashtable.put(time, new Plane(name, time));
             numPlanes++;
-            System.out.println("added plane @ time " + time);
+            //System.out.println("added plane @ time " + time);
             // displayPlanes();
+            return "added plane @ time " + time;
 
         } else {
-            System.out.println("time conflict @ time " + time);
+            //System.out.println("time conflict @ time " + time);
+            return "time conflict @ time " + time;
         }
 
-
     }
-
-
 
     public void displayPlanes() {
         for (Map.Entry entry: hashtable.entrySet()) {
             System.out.println(entry.getValue());
         }
     }
-}
+
+    // -------------------------------------------------------------
+} // end class RunwayHashTable
