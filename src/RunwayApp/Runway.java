b82d94fcfbbc144033cee288071a008a89dbd4d5
Chris Yeomans <christopherbyeomans@gmail.com>
2017-04-13 13:17:24 -0400

    Changed runway class to an interface, and updated runwayApp with cleaner output

--- src/RunwayApp/Runway.java
+++ src/RunwayApp/Runway.java
@@ -9,47 +9,7 @@ package RunwayApp;
  *
  * @author Christopher
  */
-public class Runway {
-  final int MAX_SLOTS = 100;
-      Plane a[];
-      int numPlanes = 0;
-      
-// -------------------------------------------------------------
-   public Runway()  // constructor
-      {
-         a = new Plane[MAX_SLOTS];
-      }
-
-
-   public void addPlane(String name, int time){  
-      // will impliment check time 
-     a[numPlanes++] = new Plane(name, time);
-      }  
-
-   public void displayPlanes(){  
-        int i;
-      for(i=0; i<numPlanes; i++)    
-         {
-         
-            System.out.println(a[i]); 
-
-         }
-   
-}
-   public void insertionSort()
-      {
-      int in, out;
-
-      for(out=1; out<numPlanes; out++)     // out is dividing line
-         {
-         Plane temp = a[out];            // remove marked item
-         in = out;                      // start shifts at out
-         while(in>0 && a[in-1].getTime() >= temp.getTime()) // until one is smaller,
-            {
-            a[in] = a[in-1];            // shift item to right
-            --in;                       // go left one position
-            }
-         a[in] = temp;                  // insert marked item
-         }  // end for
-      }  // end insertionSort()
+interface Runway {
+    public String addPlane(String name, int time);
+    public void displayPlanes();
 }
--- src/RunwayApp/RunwayApp.java
+++ src/RunwayApp/RunwayApp.java
@@ -5,80 +5,101 @@
  */
 package RunwayApp;
 
+
+import java.util.concurrent.ThreadLocalRandom;
+
 /**
  *
  * @author Christopher
  */
-public class RunwayApp {
+class RunwayApp {
 
-    /**
-     * @param args the command line arguments
-     */
+    public static void runwayTime(String name, Runway runway) {
+
+        long startTime = System.currentTimeMillis();
+
+        for (int i = 1; i < 1000; i++) {
+            
+
+            int randomNum = ThreadLocalRandom.current().nextInt(1, 3000 + 1);
+            
+            runway.addPlane("Delta " + i, randomNum);
+
+        }
+
+        long endTime = System.currentTimeMillis();
+        long totalTime = endTime - startTime;
+        System.out.println("execution time for " + name + " = " + totalTime + " milliseconds");
+
+    }
+
     public static void main(String[] args) {
-        // TODO code application logic here
-       System.out.println("Runway Array");
-            RunwayArray theRunway = new RunwayArray();
 
+        System.out.println("Runway Array");
 
-            theRunway.addPlane("Delta 1023", 2);
+        RunwayArray theRunway = new RunwayArray();
 
-            theRunway.addPlane("Delta 1023", 4);
+        System.out.println(theRunway.addPlane("Delta 1023", 2));
 
-            theRunway.addPlane("Delta 1023", 3);
+        System.out.println(theRunway.addPlane("Delta 1023", 4));
+        System.out.println(theRunway.addPlane("Delta 1023", 3));
+        System.out.println(theRunway.addPlane("Delta 1023", 500));
+        System.out.println(theRunway.addPlane("Delta 1023", 498));
+        System.out.println(theRunway.addPlane("Delta 1023", 496));
+        System.out.println(theRunway.addPlane("Delta 1023", 4));
 
+        System.out.println("Runway Array has the following time schedule: ");
+        theRunway.displayPlanes();
 
-            theRunway.addPlane("Delta 1023", 500);
+        System.out.println("starting time test for Runway Array");
+        runwayTime("Runway Array", theRunway);
+        theRunway.displayPlanes();
+        
+        System.out.println();
 
-            theRunway.addPlane("Delta 1023", 498);
+        System.out.println("New Runway Hashtable");
 
-            theRunway.addPlane("Delta 1023", 4);
+        RunwayHashTable theRunway2 = new RunwayHashTable();
 
+        System.out.println(theRunway2.addPlane("Delta 1023", 2));
 
-            theRunway.addPlane("Delta 1023", 495);
-            System.out.println("Runway Array has the following time schedule: ");
-            theRunway.displayPlanes();
+        System.out.println(theRunway2.addPlane("Delta 1023", 4));
+        System.out.println(theRunway2.addPlane("Delta 1023", 3));
+        System.out.println(theRunway2.addPlane("Delta 1023", 500));
+        System.out.println(theRunway2.addPlane("Delta 1023", 498));
+        System.out.println(theRunway2.addPlane("Delta 1023", 496));
 
-            System.out.println();
-            System.out.println("New Runway Hashtable");
-                RunwayHashTable theRunway2 = new RunwayHashTable();
-         
+        System.out.println(theRunway2.addPlane("Delta 1023", 4));
 
+        System.out.println("Runway Hashtable has the following time schedule: ");
 
+        theRunway2.displayPlanes();
+        
+        System.out.println("starting time test for Runway Hash Table");
+        runwayTime("Runway Hash Table", theRunway2);
+        theRunway2.displayPlanes();
 
-            theRunway2.addPlane("Delta 1023", 492);
-            theRunway2.addPlane("Delta 1023", 494);
+        System.out.println();
 
-            System.out.println("Runway Hashtable has the following time schedule: ");
+        System.out.println("New Runway BST");
 
-            theRunway2.displayPlanes();
+        RunwayBST theRunway3 = new RunwayBST();
 
-            System.out.println();
+        System.out.println(theRunway3.addPlane("Delta 1023", 2));
 
-            System.out.println("New Runway BST");
-               RunwayBST theRunway3 = new RunwayBST();
+        System.out.println(theRunway3.addPlane("Delta 1023", 4));
+        System.out.println(theRunway3.addPlane("Delta 1023", 3));
+        System.out.println(theRunway3.addPlane("Delta 1023", 500));
+        System.out.println(theRunway3.addPlane("Delta 1023", 498));
+        System.out.println(theRunway3.addPlane("Delta 1023", 496));
+        System.out.println(theRunway3.addPlane("Delta 1023", 4));
 
+        System.out.println("Runway BST has the following time schedule: ");
+        theRunway3.displayPlanes();
 
-            theRunway3.addPlane("Delta 1023", 4);
+        System.out.println("starting time test for Runway Binary Search Tree");
+        runwayTime("Runway Binary Search Tree", theRunway3);
+       
 
-            theRunway3.addPlane("Delta 1023", 3);
-
-
-            theRunway3.addPlane("Delta 1023", 500);
-
-            theRunway3.addPlane("Delta 1023", 498);
-
-            theRunway3.addPlane("Delta 1023", 4);
-
-
-            theRunway3.addPlane("Delta 1023", 495);
-
-            System.out.println("Runway BST has the following time schedule: ");
-
-            theRunway3.displayPlanes();
-
-
-
-
-        } // end main()
-    
-}
+    } // end main()
+} // end class RunwayApp
